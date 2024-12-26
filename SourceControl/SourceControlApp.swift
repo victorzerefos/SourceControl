87e2482672590b7b398f6054de8201e49dd630cc
Victor Zerefos <victor.zerefos@gmail.com>
2024-12-26 17:53:39 -0300

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Victor Zerefos on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
